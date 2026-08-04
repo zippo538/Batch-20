81e4ea47747d91e530c2e293a13d5a11d8ea3a8e
mahindra <uchihas538.zippo@gmail.com>
2026-08-04 08:37:24 +0700

    Queue Exercise

---
+++ belajar-c#-formulatrix/Week1/Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+
+QueueManager queueManager = new QueueManager();
+
+queueManager.AddTask("Queued A");
+queueManager.AddTask("Queued B");
+queueManager.ProcessNextTask();
+queueManager.ProcessNextTask();
+
+
+public class QueueManager {
+private readonly Queue<string> _taskQueue = new Queue<string>();
+
+public void AddTask(string taskName)
+    {
+        _taskQueue.Enqueue(taskName);
+        Console.WriteLine(taskName);
+    }
+
+public void ProcessNextTask()
+    {
+        if (_taskQueue.Count == 0)
+        {
+            Console.WriteLine("Queue is empty");
+            return ;
+        }
+        string activeTask = _taskQueue.Dequeue();
+        Console.WriteLine($"proccesed {activeTask}");
+    }
+
+}
